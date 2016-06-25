553438c49847082a0c73371a64f30714afddead8
Sabryna <sabryna.sousa1323@gmail.com>
2016-06-25 00:08:34 -0300

    Apply style in controllers

--- app/controllers/superior_public_agency_controller.rb
+++ app/controllers/superior_public_agency_controller.rb
@@ -3,15 +3,15 @@
 # agencies
 class SuperiorPublicAgencyController < ApplicationController
   def show
-    @superior_agency = SuperiorPublicAgency.find(params[:id])
-    public_agencies = find_public_agencies(@superior_agency.id)
+    @superior_agency = SuperiorPublicAgency.find( params[ :id ] )
+    public_agencies = find_public_agencies( @superior_agency.id )
 
-    superior_agency_graph = [[{ 'id' => '1_', 'label' => @superior_agency.name }], []]
-    Graph.create_nodes(@superior_agency, public_agencies, superior_agency_graph)
+    superior_agency_graph = [ [ { 'id' => '1_', 'label' => @superior_agency.name } ], [  ] ]
+    Graph.create_nodes( @superior_agency, public_agencies, superior_agency_graph )
     @data_superior_agency = superior_agency_graph.to_json
   end
 
-  def find_public_agencies(superior_id)
-    public_agencies = PublicAgency.where(superior_public_agency_id: superior_id)
+  def find_public_agencies( superior_id )
+    public_agencies = PublicAgency.where( superior_public_agency_id: superior_id )
   end
 end
