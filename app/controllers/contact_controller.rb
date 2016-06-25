a426305385e54605885fc30626cb900e7a24eeeb
Sabryna <sabryna.sousa1323@gmail.com>
2016-06-24 23:59:39 -0300

    Apply style sheet in contact_controller and function_controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,9 +2,9 @@ require 'rest-client'
 # Comunicate with API to send a email
 class ContactController < ApplicationController
   def send_simple_message
-    user_email = params[:from]
-    subject = params[:subject]
-    text = params[:text]
+    user_email = params[ :from ]
+    subject = params[ :subject ]
+    text = params[ :text ]
     #puts params
     mailgun_api = Rails.application.secrets.secret_mailgun_api
     RestClient.post 'https://api:' + "#{mailgun_api}" +
@@ -15,7 +15,7 @@ class ContactController < ApplicationController
       text: text
     #puts "Emails enviado e agora redirecionando o usuário"
     respond_to do |format|
-      format.json {render json: "enviado".to_json}
+      format.json { render json: "enviado".to_json }
     end
   end
   def help
