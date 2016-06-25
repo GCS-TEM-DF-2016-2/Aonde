5a93e061dbf4aaffa0fc4a76db00a99371a132d7
Sabryna <sabryna.sousa1323@gmail.com>
2016-06-25 11:23:17 -0300

    Apply style sheet in function controller test

--- test/controllers/expense_controller_test.rb
+++ test/controllers/expense_controller_test.rb
@@ -1,3 +1,10 @@
+#####################################################################
+# Class name: ExpenseControllerTest.
+# File name: expense_controller_test.rb.
+# Description:  Test class that contains all functional tests for
+# expense controller.
+#####################################################################
+
 require 'test_helper'
 
 class ExpenseControllerTest < ActionController::TestCase
