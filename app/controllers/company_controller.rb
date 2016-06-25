675ce58c597572dcb1b4016d4f66e1b3f1f8639d
Lucas Amoêdo <lucas_advc@outlook.com>
2016-06-25 12:38:06 -0300

    Improve variable names and uncomplicated code in company_controller.rb

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -14,9 +14,9 @@ class CompanyController < ApplicationController
   def show
     # Gets from the URL the year whose attributes are being collected.
     year = params[ :year ]
-    id = params[ :id ]
+    company_id = params[ :id ]
     company_expense = HelperController
-                      .find_expenses_entity( year, id, :company, :name )
+                      .find_expenses_entity( year, company_id, :company, :name )
 
     respond_to do |format|
       format.json { render json: company_expense }
@@ -26,29 +26,30 @@ class CompanyController < ApplicationController
   # Description: Finds the expenses and hiring incidence of a company.
   # Parameters: none.
   # Return: none.
-  def find
-    expenses = Expense.where( company_id: params[ :id ] )
-    company_hiring_incidence = find_public_agencies( expenses )
-    # company_hiring_incidence = get_15_first_nodes( company_hiring_incidence )
+ def find
+    company_expenses = Expense.where( company_id: params[ :id ] )
+    company_hiring_incidence = find_public_agencies( company_expenses )
     @company = Company.find( params[ :id ] )
-    data_company = generate_company_node( @company.name )
-    array = generate_public_agency_node( @company.name, company_hiring_incidence,
-                                        data_company )
-    @correct_datas = array.to_json
+    company_node = generate_company_node( @company.name )
+    company_data = generate_public_agency_node( @company.name, 
+                                        company_hiring_incidence, company_node )
+    @correct_datas = company_data.to_json
   end
 
   # Description: Finds and sorts the public agencies based on their expenses.
   # Parameters: expenses.
-  # Return: none.
-  def find_public_agencies( expenses )
+  # Return: company_hiring_incidence.
+   def find_public_agencies( expenses )
     company_hiring_incidence = {}
     expenses.each do |expense|
-      unless expense.public_agency_id.nil?
+      if (!expense.public_agency_id.nil?)
         public_agency = PublicAgency.find( expense.public_agency_id )
         verify_insert( company_hiring_incidence, public_agency )
+      else 
+        # Nothing to do.
       end
     end
-    company_hiring_incidence.sort_by { |_name, expense| expense }
+   company_hiring_incidence.sort_by { |_name, expense| expense }
   end
 
   # Description: Gets the hiring count of a given public agency.
@@ -56,22 +57,27 @@ class CompanyController < ApplicationController
   # Return: counting.
   def find_hiring_count( public_agency )
     counting = Expense.where( public_agency_id: public_agency.id ).count
+    return counting
   end
 
-  # Description: Verifies and stores the hiring incidence counting of a company.
+  # Description: Stores the hiring incidence counting of a company in a hash,
+  # but only if there was no previously value stored for that company.
   # Parameters: company_hiring_incidence, public_agency.
   # Return: none.
   def verify_insert( company_hiring_incidence, public_agency )
-    unless company_hiring_incidence[ public_agency.name ]
+    if ( company_hiring_incidence[ public_agency_name ].nil? )
       counting = find_hiring_count( public_agency )
       company_hiring_incidence[ public_agency.name ] = counting
+    else
+      # Nothing to do.
     end
+    return company_hiring_incidence
   end
 
   # Description: Prepares a 'node' of a company. This node is a collection of 
   # data in hash format to be used in the charts.
   # Parameters: company_name.
-  # Return: none.
+  # Return: data_company.
   def generate_company_node( company_name )
     data_company = [
       { 'data' => { 'id' => company_name }, 'position' => { 'x' => 0,
@@ -79,6 +85,7 @@ class CompanyController < ApplicationController
       { 'data' => { 'id' => 'Órgãos Públicos' } },
       { 'data' => { 'id' => 'qtde Contratações' } }
      ]
+     return data_company;
   end
   # Description: Prepares a 'node' of a public agency. This node is a collection
   # of data in hash format to be used in the charts.
@@ -114,7 +121,7 @@ class CompanyController < ApplicationController
     array_general << edges
   end
 
-  private :generate_public_agency_node,:generate_company_node,:find_hiring_count,:find_public_agencies,
-          :verify_insert
+  private :generate_public_agency_node, :generate_company_node,
+          :find_hiring_count, :find_public_agencies, :verify_insert
 
 end
