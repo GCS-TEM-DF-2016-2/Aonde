b68603168620e580677b3c1e8c9974bc2a9e4b95
Sabryna <sabryna.sousa1323@gmail.com>
2016-06-25 11:01:47 -0300

    Aply style sheet and add header in budget api test and appication controller test

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,8 +1,8 @@
 #####################################################################
 # Class name: ApplicationControllerTest.
 # File name: application_controller_test.rb.
-# Description: Controller that contains all the functionals test
-# of application.
+# Description:  Test class that contains all functional tests for
+# application controller.
 #####################################################################
 
 require 'test_helper'
@@ -10,18 +10,18 @@ require 'test_helper'
 class ApplicationControllerTest < ActionController::TestCase
 		test "Should find public agency" do
 				create_entities
-				@controller.find_agency_by_id(1)
-				assert assigns(:public_agency)
+				@controller.find_agency_by_id( 1 )
+				assert assigns( :public_agency )
 		end
 
 		test "Should find superior public agency" do
 				create_entities
-				@controller.find_agency_by_id(1)
-				assert assigns(:superior_public_agency)
+				@controller.find_agency_by_id( 1 )
+				assert assigns( :superior_public_agency )
 		end
 
 		def create_entities
-				SuperiorPublicAgency.create(id: 1, name: 'valid SuperiorPublicAgency')
-		   		PublicAgency.create(id: 1, views_amount: 0, name: 'valid Agency', superior_public_agency_id: 1)
+				SuperiorPublicAgency.create( id: 1, name: 'valid SuperiorPublicAgency' )
+		   		PublicAgency.create( id: 1, views_amount: 0, name: 'valid Agency', superior_public_agency_id: 1 )
 		end
 end
