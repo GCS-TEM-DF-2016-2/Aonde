d6cb03b78fd7062e5cb20f457f88aa6981790c65
Lucas Amoêdo <lucas_advc@outlook.com>
2016-06-26 16:54:26 -0300

    Add method headers to public_agency_controller.rb

--- app/controllers/public_agency_controller.rb
+++ app/controllers/public_agency_controller.rb
@@ -5,10 +5,11 @@
 # views of public_agency and the model PublicAgency.
 #####################################################################
 
-# public_agency.rb
-# Create a class to handle the data related with public agencies
 class PublicAgencyController < ApplicationController
-    # list of all public agencies in DB
+    
+    # Description: Lists all public agencies in the database.
+    # Parameters: none.
+    # Return: none.
     def index
         @public_agencies = PublicAgency.all
         @total_expense_agency = {}
@@ -18,7 +19,10 @@ class PublicAgencyController < ApplicationController
         end
     end
 
-    # Find the data of one public agency to show in the view with chart
+    # Description: Finds the data of one public agency to show in the view 
+    # through a chart.
+    # Parameters: none.
+    # Return: none.
     def show
         find_agency_by_id( params[ :id ] )
         increment_views_amount( @public_agency )
@@ -26,6 +30,9 @@ class PublicAgencyController < ApplicationController
 
     public
 
+    # Description: Prepares the data to be shown in an Agency chart.
+    # Parameters: none.
+    # Return: none.
     def agency_chart
         params[ :year ] = '2015' unless params[ :year ]
         get_expenses_for_public_agency = HelperController.expenses_year( params[ :id ]
@@ -38,21 +45,30 @@ class PublicAgencyController < ApplicationController
         end
     end
 
-    # methods that's need to be private
     private
 
+    # Description: Controls the "Viewed" counter of an agency. Increases the 
+    # counter by one each time the agency is visited.
+    # Parameters: public_agency
+    # Return: none.
     def increment_views_amount( public_agency )
         views_amount = public_agency.views_amount
         views_amount += 1
         public_agency.update( views_amount: views_amount )
     end
 
+    # Description: reunites the expenses of a given public agency.
+    # Parameters: id_public_agency
+    # Return: total_expense
     def get_expenses_for_public_agency( id_public_agency )
         total_expense = Expense.where( public_agency_id: id_public_agency ).sum( :value )
 
         return total_expense
     end
 
+    # Description: Creates a hash of temporary expenses.
+    # Parameters: expenses_month, year
+    # Return: temporary_expense
     def change_type_list_expenses( expenses_month, year )
         HelperController.int_to_month( expenses_month )
         temporary_expense = { year => expenses_month }
