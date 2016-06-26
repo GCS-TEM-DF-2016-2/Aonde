8d3db204170c0872c529ab86235f2c0bf54cfc5b
Lucas Amoêdo <lucas_advc@outlook.com>
2016-06-26 19:28:07 -0300

    Add assertions to helper_controller.rb

--- app/controllers/helper_controller.rb
+++ app/controllers/helper_controller.rb
@@ -4,6 +4,7 @@
 # Description: Contain some methods to help process data in controllers
 #######################################################################
 module HelperController
+  extend Assertions
 
   MONTHNAMES_BR = [ nil ] + %w( Janeiro Fevereiro Março Abril Maio Junho
                              Julho Agosto Setembro Outubro Novembro Dezembro )
@@ -12,6 +13,8 @@ module HelperController
   # Parameters: expense_of_a_month.
   # Return: expense_of_a_month.
   def self.int_to_month( expense_of_a_month )
+    assert_object_is_not_null( expense_of_a_month )
+    assert_type_of_object( expense_of_a_month, Hash )
     expense_of_a_month.transform_keys! do |month|
       MONTHNAMES_BR[ month ]
     end
@@ -23,16 +26,24 @@ module HelperController
   # Parameters: id_public_agency, year.
   # Return: expense_year.
   def self.expenses_year( id_public_agency, year )
+    assert_object_is_not_null( id_public_agency )
+    assert_type_of_object( id_public_agency, String )
+    assert_object_is_not_null( year )
+    assert_type_of_object( year, String )
     expense_year = Expense.where( public_agency_id: id_public_agency,
-                                 payment_date: "#{year}-01-01".."#{year}-12-31" )
+                                payment_date: "#{year}-01-01".."#{year}-12-31" )
                    .group( 'MONTH( payment_date )' ).sum( :value )
-    expense_year
+    return expense_year
   end
 
   # Description: Finds the expenses of a given entity.
   # Parameters: year, id, name_entity, attribute.
   # Return: none.
   def self.find_expenses_entity( year = '2015', id, name_entity, attribute )
+    assert_object_is_not_null( id )
+    assert_type_of_object( id, Fixnum )
+    assert_object_is_not_null( attribute )
+    assert_type_of_object( attribute, Hash )
     if year.nil?
      year = 2015
     else
@@ -51,8 +62,11 @@ module HelperController
   # Return: date.
   def self.create_date( date = { from_month: 'Janeiro', end_month: 'Dezembro',
                                 from_year: 2009, end_year: 2020 } )
+    assert_type_of_object( date, Hash )
     first_month = month_to_int( date[ :from_month ] )
+    assert_object_is_not_null( first_month )
     last_month = month_to_int( date[ :end_month ] )
+    assert_object_is_not_null( last_month )
     first_date = Date.new( date[ :from_year ].to_i, first_month, 1 )
     last_date = Date.new( date[ :end_year ].to_i, last_month, 1 )
 
@@ -66,7 +80,10 @@ module HelperController
   # Parameters: month.
   # Return: index.
   def self.month_to_int( month )
+    assert_object_is_not_null( month )
+    assert_type_of_object( month, String )
     index = MONTHNAMES_BR.index( month )
+    assert_object_is_not_null( index )
     return index
   end
 
@@ -74,6 +91,10 @@ module HelperController
   # Parameters: begin_date, end_date
   # Return: validation_status
   def self.date_valid?( begin_date, end_date )
+    assert_object_is_not_null( begin_date )
+    assert_type_of_object( begin_date, Date )
+    assert_object_is_not_null( end_date )
+    assert_type_of_object( end_date, Date )
     validation_status = true
 
     if ( begin_date.year == end_date.year )
@@ -94,6 +115,8 @@ module HelperController
   # Parameters: date.
   # Return: last_day.
   def self.last_day_date( date )
+    assert_object_is_not_null( date )
+    assert_type_of_object( date, Date )
     month = date.month
     last_day = 0
     if month == 2
@@ -111,6 +134,8 @@ module HelperController
   # Parameters: date.
   # Return: day.
   def self.leap_year_day( date )
+    assert_object_is_not_null( date )
+    assert_type_of_object( date, Date )
     day = 0
     if date.leap?
       day = 29
