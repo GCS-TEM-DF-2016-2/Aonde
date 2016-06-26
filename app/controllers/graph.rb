e0e022ecda639ff2168f74c96f1202decf7df9a0
Matheus Miranda <matheusmirandalacerda@gmail.com>
2016-06-26 20:34:09 -0300

    Add api comments to graph_controller

--- app/controllers/graph.rb
+++ app/controllers/graph.rb
@@ -5,6 +5,9 @@
 #####################################################################
 
 module Graph
+    # Description: Generates a set of specifics nodes.
+    # Parameters: entity,data_array,graph_data.
+    # Return: none.
     def self.create_nodes( entity, data_array, graph_data )
         entity_name = entity.class.name
 
@@ -16,6 +19,9 @@ module Graph
         end
     end
 
+    # Description: Obtain a value that is associated with a name.
+    # Parameters: data,entity_name.
+    # Return: name_value.
     def self.obtain_name_value( entity_name, data )
         name = 0
         value = 1
@@ -32,6 +38,9 @@ module Graph
         return name_value
     end
 
+    # Description: Adds a specific node to the graph.
+    # Parameters: data_graph,name_value.
+    # Return: none.
     def self.create_node( data_graph, name_value )
         add_node( name_value[ :name ], data_graph, name_value[ :class_entity ], name_value[ :id ] )
         add_edge( data_graph, name_value[ :class_entity ] )
@@ -43,6 +52,9 @@ module Graph
         end
     end
 
+    # Description: Method that adds edges to the graph.
+    # Parameters: name,data_graph,name_entity,id_entity.
+    # Return: none.
     def self.add_node( name, data_graph, name_entity, id_entity )
         node = 0
         next_id = id_node( data_graph[ node ].last[ 'id' ] ) + 1
@@ -50,11 +62,19 @@ module Graph
                               'group' => name_entity }
     end
 
+    # Description: Remove a character from and convert the id to a number.
+    # Parameters: full_id.
+    # Return: id.
     def self.id_node( full_id )
         id = full_id.split( '_' )
         id[ 0 ].to_i
+
+        return id
     end
 
+    # Description: Method that adds edges to the graph.
+    # Parameters: data_graph, class_entity.
+    # Return: none.
     def self.add_edge( data_graph, class_entity )
         node = 0
         last_id = data_graph[ node ].last[ 'id' ]
@@ -63,6 +83,9 @@ module Graph
         data_graph[ edge ] << { 'from' => '1_', 'to' => last_id, 'color' => color }
     end
 
+    # Description: Add the value of a public agency to the graph.
+    # Parameters: value,data_graph.
+    # Return: none.
     def self.add_value( value, data_graph )
         edge = 1
         if value >= 0
@@ -76,6 +99,9 @@ module Graph
         end
     end
 
+    # Description: Define a specific color to element according to its class.
+    # Parameters: class_entity.
+    # Return: color.
     def self.color_edge( class_entity )
         color = nil
         if class_entity == PublicAgency.name
