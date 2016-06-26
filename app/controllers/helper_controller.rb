2331efaf8a4899d2bd45bae2e90b42d542db8564
Lucas Amoêdo <lucas_advc@outlook.com>
2016-06-26 18:55:28 -0300

    Add method headers to helper_controller.rb

--- app/controllers/helper_controller.rb
+++ app/controllers/helper_controller.rb
@@ -1,14 +1,25 @@
-# helper_controller.rb
-# Contain some method to helper the process of data in controllers
+######################################################################
+# Class name: HelperController
+# File name: helper_controller.rb
+# Description: Contain some methods to help process data in controllers
+#######################################################################
 module HelperController
+
   MONTHNAMES_BR = [ nil ] + %w( Janeiro Fevereiro Março Abril Maio Junho
                              Julho Agosto Setembro Outubro Novembro Dezembro )
+  # Description: Allocates expenses to months and stores them in a hash.
+  # Parameters: expense_month.
+  #Return: expense_month.
   def self.int_to_month( expense_month )
     expense_month.transform_keys! do |month|
       MONTHNAMES_BR[ month ]
     end
   end
 
+  # Description: Gets all the expenses of a given public agency, sums them all, 
+  # and associates them to a given year.
+  # Parameters: id_public_agency, year.
+  # Return: expense_year.
   def self.expenses_year( id_public_agency, year )
     expense_year = Expense.where( public_agency_id: id_public_agency,
                                  payment_date: "#{year}-01-01".."#{year}-12-31" )
@@ -16,6 +27,9 @@ module HelperController
     expense_year
   end
 
+  # Description: Finds the expenses of a given entity.
+  # Parameters: year, id, name_entity, attribute.
+  # Return: none.
   def self.find_expenses_entity( year = '2015', id, name_entity, attribute )
    year ||= 2015
    Expense.joins( name_entity )
@@ -24,9 +38,12 @@ module HelperController
       .sum( :value ).transform_values! {|v| v.to_f}.to_a
   end
 
+  # Description: Creates a date, in hash form, based on parameters provided by
+  # the user.
+  # Parameters: date.
+  # Return: date.
   def self.create_date( date = { from_month: 'Janeiro', end_month: 'Dezembro',
                                 from_year: 2009, end_year: 2020 } )
-    # puts date
     first_month = month_to_int( date[ :from_month ] )
     last_month = month_to_int( date[ :end_month ] )
     first_date = Date.new( date[ :from_year ].to_i, first_month, 1 )
@@ -34,10 +51,13 @@ module HelperController
 
     last_date = last_day_date( last_date )
     date = { begin: first_date, end: last_date }
-    # puts date
     date
   end
 
+  # Description: Returns the index of a given month in the MONTHS_BR array. 
+  # Basically, returns the position of a month in the year.
+  # Parameters: month.
+  # Return: none.
   def self.month_to_int( month )
     MONTHNAMES_BR.index( month )
   end
@@ -55,6 +75,9 @@ module HelperController
     valid
   end
 
+  # Description: Gets the last day of month.
+  # Parameters: date.
+  # Return: last_day.
   def self.last_day_date( date )
     month = date.month
 
@@ -70,6 +93,10 @@ module HelperController
     date.change( day: last_day )
   end
 
+  # Description: Checks if the year of a given date is a leap year, and adjust
+  # the day accordingly.
+  # Parameters: date.
+  # Return: day.
   def self.leap_year_day( date )
     day = 0
     if date.leap?
@@ -79,5 +106,7 @@ module HelperController
     end
     day
   end
-private_class_method :leap_year_day,:last_day_date,:date_valid?,:month_to_int
+
+  private_class_method :leap_year_day,:last_day_date,:date_valid?,:month_to_int
+
 end
