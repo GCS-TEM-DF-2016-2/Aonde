e43931ae98046dbb7cfbf7a493523b90274e238e
ArthurTemporim <arthurrtl@gmail.com>
2016-06-26 22:01:15 -0300

    Add comments to expense controller.

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -1,2 +1,8 @@
+######################################################################
+# Class name: ExpenseController
+# File name: expense_controller.rb
+# Description: This controller manage the expense data.
+#######################################################################
+
 class ExpenseController < ApplicationController
 end
