193a719f4faa045f1fa29e16755d09fef05ff1d5
ArthurTemporim <arthurrtl@gmail.com>
2016-06-26 22:02:48 -0300

    Add comments to search controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,6 @@ class ApplicationController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :null_session
-    #  before_action :owl_zueira
-
-    # def owl_zueira
-    #  logger.info "  ,,,\n {0,0}\n./ ) )\n==\"=\"=="
-    # sleep( 1 )
-    # end
 
     public
 
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,52 +1,61 @@
 #####################################################################
-# Module name: SearchController
+# Class name: TypeExpenseController
 # File name: search_controller.rb
-# Description: Contains all the methods to make search work.
+# Description: Process the request of search for a key word for
+# public agency, programs or companies.
 #####################################################################
 
 class SearchController < ApplicationController
+
+    # Description: This method is called to make view of this class.
+    # Parameters: none.
+    # Return: none.
     def index
         search = params[:search]
         @results = {}
         if search.tr( ' ', '' ).length > 4
-            @results = find_entities( search )
+          @results = find_entities( search )
         else
-            logger.warn 'Try search with less the 5 characters'
+          logger.warn 'Try search with less the 5 characters'
         end
     end
 
-    # Description: Recovers an array with all the object for each
-    # types of entities.
+    # Description: This method find public agencys with one string to search.
     # Parameters: keyword.
     # Return: entities.
     def find_entities( keyword )
-        public_agencies = search_entities( PublicAgency, :public_agency_id, keyword )
+        public_agencies = search_entities( PublicAgency, :public_agency_id,
+         keyword )
         programs = search_entities( Program, :program_id, keyword )
         companies = search_entities( Company, :company_id, keyword )
-        entities = { agency: public_agencies, program: programs, company: companies }
-
+        entities = { agency: public_agencies, program: programs, 
+          company: companies }
         return entities
     end
 
-    # Description: Returns all the expenses for a specific entity.
-    # Parameters: class_entity,name_field,keyword.
-    # Return: total_expense_entity.
+    # Description: This method search expenses of an especifuc public agency.
+    # Parameters: class_entity, name_field, keyword.
+    # Return: array_total_expense_entity.
     def search_entities( class_entity, name_field, keyword )
-        entities = class_entity.select( :name, :id ).where( 'name LIKE ?', "%#{keyword}%" )
+        entities = class_entity.select( :name, :id ).where( 'name LIKE ?',
+         "%#{keyword}%" )
         total_expense_entity = {}
         entities.each do |entity|
-            total_expense_entity[entity] = expense_entities( name_field, entity.id )
+          total_expense_entity[entity] = expense_entities( name_field,
+           entity.id )
         end
 
-        return total_expense_entity.to_a
+        array_total_expense_entity = total_expense_entity.to_a
+
+        return array_total_expense_entity
     end
 
-    # Description: Returns all the expenses for a type of entity.
-    # Parameters: name_field,entity_id.
+    # Description: This method take the sum of all expenses with an especific
+    # id.
+    # Parameters: name_field, entity_id.
     # Return: total_expense.
     def expense_entities( name_field, entity_id )
         total_expense = Expense.where( name_field => entity_id ).sum( :value )
-
         return total_expense
     end
 
